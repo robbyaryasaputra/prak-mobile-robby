e9d313a95ce0eed08de2e68b2a9908075a99418d
rafi24si <rafi24si@mahasiswa.pcr.ac.id>
2026-05-22 10:36:45 +0700

    List view dan ListView (custom Aapter)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -39,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
